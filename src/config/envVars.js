8b4bd939e6250ee4edba05ac6afee352db4a183b
Vahagn Saribekyan <Vahag>
2021-09-06 00:15:38 +0400

    Removing unneseccary env variables

--- src/config/envVars.js
+++ src/config/envVars.js
@@ -17,11 +17,6 @@ module.exports = {
     name: 'LOG_LEVEL',
     validator: str({choices: ['error', 'warn', 'info', 'verbose', 'debug', 'silly']})
   },
-  /** ************** Database connection params *********************/
-  dbQueryTimeout: {
-    name: 'DB_QUERY_TIMEOUT',
-    validator: num()
-  },
   /** ************** AWS credentials *********************/
   awsAccessKeyId: {
     name: 'AWS_ACCESS_KEY_ID',
